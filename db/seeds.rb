c215c82863f0135d8c4dbe6376e0f262687732ea
Sumit@98280 <sumit.patel@housing.com>
2023-07-05 13:31:53 +0530

    TASK-1: First_ROR

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+  # Article.create([{ title: "Star Wars", author: "Sumit" },{title: "Helen Keller", author: "xyz"}])
+   
